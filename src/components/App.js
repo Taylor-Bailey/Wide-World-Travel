f36efdd5c4e64b8a83a5579477895ade247a2c5b
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-04-06 15:17:28 -0500

    added background image

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './Header';
+import Image from './Image';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header title="Wide World Travel, Inc"/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Image />
       </div>
     );
   }
