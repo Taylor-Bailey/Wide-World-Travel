208056ece91adbdfa45814e6d91a6986ab97fa1d
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-04-09 15:43:37 -0500

    created image component and passed props.bg through it to render to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './Header';
 import Image from './Image';
+import bg from '../images/sci.png'
+import Footer from './Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header title="Wide World Travel, Inc"/>
-        <Image />
+        <Image bg={bg} />
         <Footer />
       </div>
     );
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer(props) {
+    return(
+        <footer className="App-footer">
+            <ul>
+                <li>React XVIII</li>
+                <li>The Infinite Nest</li>
+                <li>Boreal Bootstrap</li>
+                <li>Modular Magnetar 8i30-xC</li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer;
